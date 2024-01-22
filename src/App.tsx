462331658167b808165211fae6a6bebd84de12c9
Igor Kulka <kulkaigo1911@gmail.com>
2024-01-22 15:13:07 +0100

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './momence-logo.svg';
+import logoTypography from './momence-logo-typography.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-logo-container" href='https://momence.com/'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logoTypography} className="App-logo-typography" alt="logo-typography" />
         </a>
+        <h1 className='App-title'>
+          Currency converter
+        </h1>
+        <p className='App-description'>Tech task assignment</p>
+        <div style={{ display: 'block', width: '50vw', height: '20vh', background: 'var(--white)', borderRadius: '24px', color: 'black' }}>calculator goes here</div>
       </header>
+      <section className='App-content'>
+        <div style={{ display: 'block', width: '50vw', height: '40vh', background: 'var(--white-smoke)', borderRadius: '24px' }}>Table goes here</div>
+      </section>
     </div>
   );
 }
